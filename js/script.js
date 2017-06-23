89cc80206d03f2a081782b18c13cf133b970be0b
Gustavojs3 <gustavo.jspontes@hotmail.com>
2017-06-23 09:32:32 -0300

    Alterando (em todos os arquivos)

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function criarNomeCompleto() {
+    var nome = "Gustavo";
+    var sobrenome = "de Jesus";
+    
+    var nomeCompleto = nome + " " + sobrenome;
+    return nomeCompleto;
+}
+
+function soma(n1, n2, n3, n4) {
+    
+    var mais = (n1 + n2 + n3 + n4 ) / 4;
+    
+    return mais;
+}
